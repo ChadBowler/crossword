4302143b3fdfbd50db16883d6ca1f9735c50a8dd
ChadBowler <cbowlerdev@gmail.com>
2024-06-18 16:11:06 -0600

    rendered numbers and gray tiles to board

--- game.py
+++ game.py
@@ -3,10 +3,10 @@ import pygame
 from crossword_data import generate_new_puzzle
 from puzzle_board import structure_puzzle
 
-gen_puzzle = generate_new_puzzle()
-puzzle = structure_puzzle(gen_puzzle)
-board_height = puzzle._rows * puzzle._tile_size_y
-board_width = puzzle._cols * puzzle._tile_size_x
+# gen_puzzle = generate_new_puzzle()
+# puzzle = structure_puzzle(gen_puzzle)
+# board_height = puzzle._rows * puzzle._tile_size_y
+# board_width = puzzle._cols * puzzle._tile_size_x
 
 # pygame setup
 pygame.init()
@@ -16,8 +16,25 @@ pygame.display.set_caption('Crossword Game')
 clock = pygame.time.Clock()
 running = True
 
-color = (0,150,255)
+light_blue = (0,150,255)
+light_gray = "#A6A6A6"
+dark_gray = "#343840"
+number_font = pygame.font.Font(None, 24)
 font = pygame.font.Font(None, 36)
+
+def new_puzzle():
+    gen_puzzle = generate_new_puzzle()
+    puzzle = structure_puzzle(gen_puzzle)
+    board_height = puzzle._rows * puzzle._tile_size_y
+    board_width = puzzle._cols * puzzle._tile_size_x
+    board = pygame.Surface.subsurface(screen, ((puzzle._x1, puzzle._y1), (board_width, board_height)))
+    return board, puzzle
+
+try:
+    board, puzzle = new_puzzle()
+    
+except ValueError:
+    board, puzzle = new_puzzle()
 
 while running:
     # poll for events
@@ -28,25 +45,25 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
-    # pygame.draw.line(screen, color, (50, 50), (50, 500), width=3)
-    # print(puzzle._tiles)
-    board = pygame.Surface.subsurface(screen, ((puzzle._x1, puzzle._y1), (board_width, board_height)))
+
     board.fill("white")
     tiles = []
     for row in puzzle._tiles:
         for tile in row:
+            number = number_font.render(str(tile.base_value), True, (0, 0, 0))
             text = font.render(str(tile.answer_value), True, (0, 0, 0))
-            board.blit(text, (tile.center_point.x - puzzle._x1, tile.center_point.y - puzzle._y1))
-            # square = pygame.draw.rect(board, "red", [tile.tl_corner.x, tile.tl_corner.y, puzzle._tile_size_x, puzzle._tile_size_y])
-            # square = pygame.Surface.subsurface(board, ((tile.tl_corner.x + 10, tile.tl_corner.y + 10), (puzzle._tile_size_x - 20, puzzle._tile_size_y - 20)))
-            # pygame.Surface.blit(square, board, (tile.tl_corner.x, tile.tl_corner.y))
+            if tile.answer_value == ".":
+                square = pygame.draw.rect(board, dark_gray, [tile.tl_corner.x - puzzle._x1, tile.tl_corner.y - puzzle._y1, puzzle._tile_size_x, puzzle._tile_size_y])
+            # else:
+            #     board.blit(text, (tile.center_point.x - puzzle._x1 - 5, tile.center_point.y - puzzle._y1 - 5))
+            if tile.base_value != 0:
+                board.blit(number, (tile.tl_corner.x - puzzle._x1 + 3, tile.tl_corner.y - puzzle._y1 + 3))
 
-            pygame.draw.line(screen, color, (tile.tl_corner.x, tile.tl_corner.y), (tile.bl_corner.x, tile.bl_corner.y), width=3)
-            pygame.draw.line(screen, color, (tile.tl_corner.x, tile.tl_corner.y), (tile.tr_corner.x, tile.tr_corner.y), width=3)
-            pygame.draw.line(screen, color, (tile.tr_corner.x, tile.tr_corner.y), (tile.br_corner.x, tile.br_corner.y), width=3)
-            pygame.draw.line(screen, color, (tile.br_corner.x, tile.br_corner.y), (tile.bl_corner.x, tile.bl_corner.y), width=3)
+            pygame.draw.line(screen, light_blue, (tile.tl_corner.x, tile.tl_corner.y), (tile.bl_corner.x, tile.bl_corner.y), width=3)
+            pygame.draw.line(screen, light_blue, (tile.tl_corner.x, tile.tl_corner.y), (tile.tr_corner.x, tile.tr_corner.y), width=3)
+            pygame.draw.line(screen, light_blue, (tile.tr_corner.x, tile.tr_corner.y), (tile.br_corner.x, tile.br_corner.y), width=3)
+            pygame.draw.line(screen, light_blue, (tile.br_corner.x, tile.br_corner.y), (tile.bl_corner.x, tile.bl_corner.y), width=3)
 
-    # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
     pygame.display.flip()
