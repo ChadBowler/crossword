5a6cb9c11dee4bc212f64a7b8f950df6cb75ea3b
ChadBowler <cbowlerdev@gmail.com>
2024-06-19 17:40:59 -0600

    added event handling to move focus square with arrow keys

--- game.py
+++ game.py
@@ -65,9 +65,68 @@ def handle_keyboard_input():
                 if tile.focus:
                     if not tile.locked:
                         tile.input = ""
-    else:
-        if event.key == pygame.K_LEFT:
-            print("left")
+    elif event.key == pygame.K_LEFT:
+        for i in range(len(puzzle._tiles)-1, -1, -1):
+            for j in range(len(puzzle._tiles[i])-1, -1, -1):
+                if next_focus:
+                    if not puzzle._tiles[i][j].blank:
+                        puzzle._tiles[i][j].focus = True
+                        next_focus = False
+                        return
+                if puzzle._tiles[i][j].focus:
+                    if i == 0 and j == 0:
+                        clear_focus()
+                        puzzle._tiles[-1][-1].focus = True
+                        break
+                    clear_focus()
+                    next_focus = True
+    elif event.key == pygame.K_RIGHT:
+        for i in range(len(puzzle._tiles)):
+            for j in range(len(puzzle._tiles[i])):
+                if next_focus:
+                    if not puzzle._tiles[i][j].blank:
+                        puzzle._tiles[i][j].focus = True
+                        next_focus = False
+                        return
+                if puzzle._tiles[i][j].focus:
+                    if i == len(puzzle._tiles) - 1 and j == len(puzzle._tiles[i]) - 1:
+                        clear_focus()
+                        puzzle._tiles[0][0].focus = True
+                        break
+                    clear_focus()
+                    next_focus = True
+    elif event.key == pygame.K_DOWN:
+        for i in range(len(puzzle._tiles)):
+            for j in range(len(puzzle._tiles[i])):
+                if puzzle._tiles[i][j].focus:
+                    clear_focus()
+                    next_focus = True
+                    while next_focus:
+                        if i == len(puzzle._tiles) - 1:
+                            i = -1
+                        i += 1
+                        if not puzzle._tiles[i][j].blank:
+                            puzzle._tiles[i][j].focus = True
+                            next_focus = False
+                            return
+    elif event.key == pygame.K_UP:
+        for i in range(len(puzzle._tiles) - 1, -1, -1):
+            for j in range(len(puzzle._tiles[i]) - 1, -1, -1):
+                if puzzle._tiles[i][j].focus:
+                    clear_focus()
+                    next_focus = True
+                    while next_focus:
+                        if i == -1:
+                            i = len(puzzle._tiles) - 1
+                        i -= 1
+                        if not puzzle._tiles[i][j].blank:
+                            puzzle._tiles[i][j].focus = True
+                            next_focus = False
+                            return
+                        
+                        
+                    
+            # print("left")
         # print(event)
 
 def draw_square(square_type):
